e061cd0503542f9ba791d0ed5e394f77552b9f1b
ndvssankar <sivashankar@msitprogram.net>
2018-09-27 20:36:23 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- M4/4.1 Balanced Paranthesis/ParenthesesClient.java
+++ M4/4.1 Balanced Paranthesis/ParenthesesClient.java
@@ -56,6 +56,8 @@ public class ParenthesesClient {
 			}
 			if (j == line.length() && stack.isEmpty() == true) {
 				System.out.println("YES");
+			} else {
+				System.out.println("NO");
 			}
 		}
  	}
