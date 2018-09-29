3f1a881699b14de42daaecad2ca95d4c8896a74b
ndvssankar <sivashankar@msitprogram.net>
2018-09-29 11:57:35 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- M6/Assignment-1/Solution.java
+++ M6/Assignment-1/Solution.java
@@ -7,13 +7,15 @@ import java.util.Iterator;
  */
 class AddLargeNumbers {
 
+    public static final int TEN = 10;
+
     /**
      * Converting the number to digits.
      * @param  number a String version of the number.
      * @return returns the list contains the digits where each
      * digit is a node.
      */
-    public static LinkedList numberToDigits(String number) {
+    public static LinkedList numberToDigits(final String number) {
         LinkedList<String> ll = new LinkedList<String>();
         for (int i = 0; i < number.length(); i++) {
             ll.insert(number.charAt(i) + "");
@@ -26,16 +28,16 @@ class AddLargeNumbers {
      * @param  list containing the digits
      * @return the String version of the list
      */
-    public static String digitsToNumber(LinkedList<String> list) {
+    public static String digitsToNumber(final LinkedList<String> list) {
         return list.toString();
     }
 
     /**
-     * Converting the list to stack
-     * @param  lst list containing the digits
+     * Converting the list to stack.
+     * @param  lst list containing the digits.
      * @return the stack that contains digits.
      */
-    public static Stack<Integer> createStack(LinkedList<String> lst) {
+    public static Stack<Integer> createStack(final LinkedList<String> lst) {
         Stack<Integer> stack = new Stack<Integer>();
         Iterator<String> iter = lst.iterator();
         while (iter.hasNext()) {
@@ -52,18 +54,19 @@ class AddLargeNumbers {
      * Adding large numbers.
      * @param  list1 list containing digits of the first number.
      * @param  list2 list containing digits of the second number.
-     * @return returns the list containing the digits of the result.
+     * @return returns the list containing the digits of result.
      */
-    public static LinkedList addLargeNumbers(LinkedList<String> list1, LinkedList<String> list2) {
+    public static LinkedList addLargeNumbers(final LinkedList<String> list1,
+            final LinkedList<String> list2) {
         Stack<Integer> aStack = createStack(list1);
         Stack<Integer> bStack = createStack(list2);
         LinkedList<String> result = new LinkedList<String>();
         int flag = 0;
-        while (! (aStack.isEmpty() && bStack.isEmpty())) {
+        while (!(aStack.isEmpty() && bStack.isEmpty())) {
             int sum = aStack.pop() + bStack.pop() + flag;
-            if (sum >= 10) {
+            if (sum >= TEN) {
                 flag = 1;
-                sum = sum - 10;
+                sum = sum - TEN;
             } else {
                 flag = 0;
             }
@@ -80,6 +83,13 @@ class AddLargeNumbers {
  * Solution class to demonstrate the addition of two big numbers.
  */
 public final class Solution {
+
+    /**
+     * default constructor.
+     */
+    private Solution() {
+
+    }
     /**
      * Main Method...
      * @param args command line args
