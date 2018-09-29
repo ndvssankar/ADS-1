8f1537eb4bfdbb4c66ac798e277529416aae6d54
ndvssankar <sivashankar@msitprogram.net>
2018-09-29 15:35:12 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

---
+++ M6/Assignment-1/LinkedList.java
@@ -0,0 +1,73 @@
+import java.util.Iterator;
+
+public class LinkedList<Item> implements Iterable<Item> {
+
+	private class Node<Item> {
+		Item item;
+		Node<Item> next;
+	}
+
+	private Node<Item> first;
+	private Node<Item> last;
+
+	public Iterator<Item> iterator() {
+		return new ListIterator(this.first);
+	}
+
+	public class ListIterator<Item> implements Iterator<Item> {
+		private Node<Item> current;
+
+		public ListIterator(Node<Item> first) {
+			current = first;
+		}
+
+		public boolean hasNext() {
+			return current != null;
+		}
+
+		public Item next() {
+			Item item = (Item) current.item;
+			current = current.next;
+			return item;
+		}
+
+		public void remove() { }
+	}
+
+	public void pushFront(Item item) {
+		Node<Item> temp = new Node<Item>();
+		temp.item = item;
+		temp.next = null;
+
+		if (first == null) {
+			first = temp;
+			last = temp;
+		} else {
+			temp.next = first;
+			first = temp;
+		}
+	}
+
+	public void insert(Item item) {
+		Node<Item> temp = new Node<Item>();
+		temp.item = item;
+		temp.next = null;
+
+		if (first == null) {
+			first = temp;
+			last = temp;
+		} else {
+			last.next = temp;
+			last = temp;
+		}
+	}
+
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		Iterator<Item> iter = this.iterator();
+		while(iter.hasNext()) {
+			sb.append(iter.next());
+		}
+		return sb.toString();
+	}
+}
