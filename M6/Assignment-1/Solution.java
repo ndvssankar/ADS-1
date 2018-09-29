58cb78da6b34b75561aa07e0033164a9bc3d3392
ndvssankar <sivashankar@msitprogram.net>
2018-09-29 11:53:29 +0530

    "check_style_score -> 12 of 12 passed. style score: 0/1 "

--- M6/Assignment-1/Solution.java
+++ M6/Assignment-1/Solution.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 import java.util.Iterator;
 
+/**
+ * Adding of two Large numbers.
+ * @author Siva Sankar
+ */
 class AddLargeNumbers {
-    
+
+    /**
+     * Converting the number to digits.
+     * @param  number a String version of the number.
+     * @return returns the list contains the digits where each
+     * digit is a node.
+     */
     public static LinkedList numberToDigits(String number) {
         LinkedList<String> ll = new LinkedList<String>();
         for (int i = 0; i < number.length(); i++) {
@@ -11,14 +21,24 @@ class AddLargeNumbers {
         return ll;
     }
 
+    /**
+     * Converting digits to number.
+     * @param  list containing the digits
+     * @return the String version of the list
+     */
     public static String digitsToNumber(LinkedList<String> list) {
         return list.toString();
     }
 
+    /**
+     * Converting the list to stack
+     * @param  lst list containing the digits
+     * @return the stack that contains digits.
+     */
     public static Stack<Integer> createStack(LinkedList<String> lst) {
         Stack<Integer> stack = new Stack<Integer>();
         Iterator<String> iter = lst.iterator();
-        while(iter.hasNext()) {
+        while (iter.hasNext()) {
             try {
                 stack.push(Integer.parseInt(iter.next()));
             } catch (Exception ex) {
@@ -28,6 +48,12 @@ class AddLargeNumbers {
         return stack;
     }
 
+    /**
+     * Adding large numbers.
+     * @param  list1 list containing digits of the first number.
+     * @param  list2 list containing digits of the second number.
+     * @return returns the list containing the digits of the result.
+     */
     public static LinkedList addLargeNumbers(LinkedList<String> list1, LinkedList<String> list2) {
         Stack<Integer> aStack = createStack(list1);
         Stack<Integer> bStack = createStack(list2);
@@ -43,33 +69,43 @@ class AddLargeNumbers {
             }
             result.pushFront(sum + "");
         }
-        if (flag == 1)
+        if (flag == 1) {
             result.pushFront(flag + "");
+        }
         return result;
     }
 }
 
-public class Solution {
-    public static void main(String[] args) {
+/**
+ * Solution class to demonstrate the addition of two big numbers.
+ */
+public final class Solution {
+    /**
+     * Main Method...
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         String p = sc.nextLine();
         String q = sc.nextLine();
-        switch(input){
-            case "numberToDigits":
-                LinkedList<String> pDigits = AddLargeNumbers.numberToDigits(p);
-                LinkedList<String> qDigits = AddLargeNumbers.numberToDigits(q);
-                System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
-                System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
-                break;
+        switch (input) {
+        case "numberToDigits":
+            LinkedList<String> pDigits = AddLargeNumbers.numberToDigits(p);
+            LinkedList<String> qDigits = AddLargeNumbers.numberToDigits(q);
+            System.out.println(AddLargeNumbers.digitsToNumber(pDigits));
+            System.out.println(AddLargeNumbers.digitsToNumber(qDigits));
+            break;
 
-            case "addLargeNumbers":
-                pDigits = AddLargeNumbers.numberToDigits(p);
-                qDigits = AddLargeNumbers.numberToDigits(q);
-                LinkedList<String> result = AddLargeNumbers.addLargeNumbers(pDigits, qDigits);
-                System.out.println(AddLargeNumbers.digitsToNumber(result));
-                break;
+        case "addLargeNumbers":
+            pDigits = AddLargeNumbers.numberToDigits(p);
+            qDigits = AddLargeNumbers.numberToDigits(q);
+            LinkedList<String> result = AddLargeNumbers.addLargeNumbers(pDigits,
+                                        qDigits);
+            System.out.println(AddLargeNumbers.digitsToNumber(result));
+            break;
+        default:
+            break;
         }
     }
-    
 }
