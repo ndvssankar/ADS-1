7905efa78b7b1722893cf6e419bdbf41926447fa
ndvssankar <sivashankar@msitprogram.net>
2018-10-04 17:10:07 +0530

    "check_style_score -> 0 of 6 passed. style score: 0/1 "

---
+++ M8/8.1 Team Ranking With Insertion/Solution.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+import java.util.Arrays;
+/**
+ * Solution class for team sorting.
+ * @author Siva Sankar
+ */
+public final class Solution {
+
+    /**
+     * Constant value 3 is assigned to THREE.
+     */
+    private static final int THREE = 3;
+
+    /**
+     * default constructor.
+     */
+    private Solution() {
+
+    }
+
+    /**
+     * main method to demonstrate.
+     * @param args command line args
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = 0;
+        StringBuffer sb = new StringBuffer();
+        while (scan.hasNext()) {
+            sb.append(scan.nextLine() + "::");
+        }
+        String[] lines = sb.toString().split("::");
+        Team[] teams = new Team[lines.length];
+        for (String line : lines) {
+            String[] tokens = line.split(",");
+            teams[i++] = new Team(tokens[0],
+                                  Integer.parseInt(tokens[1]),
+                                  Integer.parseInt(tokens[2]),
+                                  Integer.parseInt(tokens[THREE]));
+        }
+        Insertion.sort(teams);
+        String output = Arrays.toString(teams).replace("[", "").
+                        replace("]", "").replace(", ", ",");
+        System.out.println(output);
+    }
+
+    // public static void main(String[] args) {
+    //     Scanner scan = new Scanner(System.in);
+    //     int i = 0;
+    //     List<Team> teams = new List<Team>();
+    //     while (scan.hasNext()) {
+    //         String[] tokens = scan.nextLine().split(",");
+    //         teams.add(new Team(tokens[0],
+    //                               Integer.parseInt(tokens[1]),
+    //                               Integer.parseInt(tokens[2]),
+    //                               Integer.parseInt(tokens[THREE])));
+    //     }
+    //     // Selection.sort(teams);
+    //     teams.sort();
+    //     System.out.println(teams);
+    // }
+}
