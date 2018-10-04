b325a9296a38695034e262211e1bc35b541998c5
ndvssankar <sivashankar@msitprogram.net>
2018-10-04 10:05:49 +0530

    "ads1_m10_a1: 0/3 testcases passed. 0 of 0 hidden testcases passed. check_style score: 0/1 "

---
+++ M10/10.1 LinkedList using Recursion/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * CLass Solution.
+ * @author Siva Sankar.
+ */
+public final class Solution {
+	/**
+	 * default constructor
+	 */
+	private Solution() {
+
+	}
+
+	/**
+	 * demonstrate linked list with recursion.
+	 * @param args command line args
+	 */
+	public static void main(final String[] args) {
+		
+	}
+}
