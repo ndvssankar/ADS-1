5c9fa6a20843c3749bcf7064162478d3ec7b3908
ndvssankar <sivashankar@msitprogram.net>
2018-10-06 12:57:03 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M12/Assignment-1/Solution.java
+++ M12/Assignment-1/Solution.java
@@ -1,106 +1,134 @@
 import java.util.Scanner;
 
-public class Solution {
+/**
+ * Solution class for alloting the seats based on the given
+ * criteria.
+ * @author Siva Sankar.
+ */
+public final class Solution {
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = Integer.parseInt(scan.nextLine());
-		int vacancies = Integer.parseInt(scan.nextLine());
-		int noCategry = Integer.parseInt(scan.nextLine());
-		int noOfBC = Integer.parseInt(scan.nextLine());
-		int noOfSC = Integer.parseInt(scan.nextLine());
-		int noOfST = Integer.parseInt(scan.nextLine());
+    /**
+     * Default constructor.
+     */
+    private Solution() {
 
-		Student[] students = new Student[N];
+    }
 
-		int count = 0;
-		while (scan.hasNext()) {
-			String[] tokens = scan.nextLine().split(",");
-			students[count++] = new Student(
-			    tokens[0], tokens[1], Integer.parseInt(tokens[2]),
-			    Integer.parseInt(tokens[3]),
-			    Integer.parseInt(tokens[4]),
-			    Integer.parseInt(tokens[5]),
-			    tokens[6]);
-		}
+    /**
+     * main method to demostrate the solution.
+     * @param args the command line arguments.
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = Integer.parseInt(scan.nextLine());
+        int vacancies = Integer.parseInt(scan.nextLine());
+        int noCategry = Integer.parseInt(scan.nextLine());
+        int noOfBC = Integer.parseInt(scan.nextLine());
+        int noOfSC = Integer.parseInt(scan.nextLine());
+        int noOfST = Integer.parseInt(scan.nextLine());
 
-		Insertion.sort(students);
-		print(students);
+        Student[] students = new Student[N];
 
-		allotment(students, vacancies, noCategry, noOfBC,
-		          noOfSC, noOfST);
-		// print(students);
-	}
+        int count = 0;
+        while (scan.hasNext()) {
+            String[] tokens = scan.nextLine().split(",");
+            students[count++] = new Student(
+                tokens[0], tokens[1], Integer.parseInt(tokens[2]),
+                Integer.parseInt(tokens[3]),
+                Integer.parseInt(tokens[4]),
+                Integer.parseInt(tokens[5]),
+                tokens[6]);
+        }
 
-	public static void print(Student[] students) {
-		for (Student student : students)
-			System.out.println(student);
-		System.out.println();
-	}
+        Insertion.sort(students);
+        print(students);
 
-	public static void allotment(Student[] students,
-	                             int vacancies, int noCategry, int noBC,
-	                             int noSC, int noST) {
-		int i = 0;
-		int k = 0;
-		int N = students.length;
-		Student[] alloted = new Student[vacancies];
+        allotment(students, vacancies, noCategry, noOfBC,
+                  noOfSC, noOfST);
+    }
 
-		for (i = 0; i < N; i++) {
-			if (vacancies == 0)
-				break;
+    /**
+     * prints the student details to the console.
+     * @param students list.
+     */
+    public static void print(Student[] students) {
+        for (Student student : students)
+            System.out.println(student);
+        System.out.println();
+    }
 
-			if (noCategry > 0) {
-				noCategry--;
-				students[i].setAlloted(true);
-				// System.out.println(students[i]);
-				alloted[k++] = students[i];
-				vacancies--;
-			}
+    /**
+     * Alloting the seats to the students based on merit.
+     * @param students  list of students.
+     * @param vacancies number of vacancies.
+     * @param noCategry Un-reserved category.
+     * @param noBC      Number of BC seats.
+     * @param noSC      Number of SC seats.
+     * @param noST      Number of ST seats.
+     */
+    public static void allotment(Student[] students,
+                                 int vacancies,
+                                 int noCategry,
+                                 int noBC,
+                                 int noSC,
+                                 int noST) {
+        int i = 0;
+        int k = 0;
+        int N = students.length;
+        Student[] alloted = new Student[vacancies];
 
-			if (noBC > 0) {
-				if (students[i].getRc().equals("BC") &&
-				        students[i].getAlloted() != true) {
-					noBC--;
-					students[i].setAlloted(true);
-					// System.out.println(students[i]);
-					alloted[k++] = students[i];
-					vacancies--;
-				}
-			}
-			if (noSC > 0) {
-				if (students[i].getRc().equals("SC") &&
-				        students[i].getAlloted() != true) {
-					noSC--;
-					students[i].setAlloted(true);
-					// System.out.println(students[i]);
-					alloted[k++] = students[i];
-					vacancies--;
-				}
-			}
-			if (noSC > 0) {
-				if (students[i].getRc().equals("ST") &&
-				        students[i].getAlloted() != true) {
-					noST--;
-					students[i].setAlloted(true);
-					// System.out.println(students[i]);
-					alloted[k++] = students[i];
-					vacancies--;
-				}
-			}
-		}
-		for (i = 0; i < N; i++) {
-			if (vacancies > 0 && students[i].getRc().equals("Open") &&
-			        students[i].getAlloted() == false) {
-				students[i].setAlloted(true);
-				// System.out.println(students[i]);
-				alloted[k++] = students[i];
-				vacancies--;
-			}
-		}
-		Insertion.sort(alloted);
-		for (Student student : alloted) {
-			System.out.println(student);
-		}
-	}
+        for (i = 0; i < N; i++) {
+            if (vacancies == 0)
+                break;
+
+            if (noCategry > 0) {
+                noCategry--;
+                students[i].setAlloted(true);
+                alloted[k++] = students[i];
+                vacancies--;
+            }
+
+            if (noBC > 0) {
+                if (students[i].getRc().equals("BC") &&
+                        students[i].getAlloted() != true) {
+                    noBC--;
+                    students[i].setAlloted(true);
+                    alloted[k++] = students[i];
+                    vacancies--;
+                }
+            }
+
+            if (noSC > 0) {
+                if (students[i].getRc().equals("SC") &&
+                        students[i].getAlloted() != true) {
+                    noSC--;
+                    students[i].setAlloted(true);
+                    alloted[k++] = students[i];
+                    vacancies--;
+                }
+            }
+
+            if (noSC > 0) {
+                if (students[i].getRc().equals("ST") &&
+                        students[i].getAlloted() != true) {
+                    noST--;
+                    students[i].setAlloted(true);
+                    alloted[k++] = students[i];
+                    vacancies--;
+                }
+            }
+        }
+
+        for (i = 0; i < N; i++) {
+            if (vacancies > 0 && students[i].getRc().equals("Open") &&
+                    students[i].getAlloted() == false) {
+                students[i].setAlloted(true);
+                alloted[k++] = students[i];
+                vacancies--;
+            }
+        }
+
+        Insertion.sort(alloted);
+        print(alloted);
+    }
 }
--- M12/Assignment-1/Student.java
+++ M12/Assignment-1/Student.java
@@ -1,121 +1,216 @@
 import java.util.Date;
 import java.text.SimpleDateFormat;
 
+/**
+ * Students class that uses Comaprable to sort the student objects.
+ * student who got more total marks will be given the priority
+ * if total marks are equal then student who got more marks 
+ * in subject 3 will be given the priority. 
+ * if subject 3 marks are equal then student who got more marks
+ * in subject 2 will be given the priority. 
+ * if subject 2 marks are equal then younger student
+ * will be given priority.
+ * @author Siva Sankar
+ */
 public class Student implements Comparable<Student> {
-	private String studentName;
-	private Date db;
-	private int m1;
-	private int m2;
-	private int m3;
-	private int tm;
-	private String rc;
-	private boolean alloted;
-	 
-	public boolean getAlloted() {
-	    return alloted;
-	}
-	 
-	public void setAlloted(boolean alloted) {
-	    this.alloted = alloted;
-	}
+    private String studentName;
+    private Date db;
+    private int m1;
+    private int m2;
+    private int m3;
+    private int tm;
+    private String rc;
+    private boolean alloted;
+    
+    /**
+     * returns true, if the student is already alloted,
+     * otherwise false.
+     * 
+     * @return true if alloted, otherwise false.
+     */
+    public boolean getAlloted() {
+        return alloted;
+    }
 
-	public Date getDb() {
-	    return db;
-	}
-	 
-	public void setDb(Date db) {
-	    this.db = db;
-	}
+    /**
+     * Changes the alloted status to true once the allotment is done.
+     * @param alloted should be either true or false.
+     */
+    public void setAlloted(boolean alloted) {
+        this.alloted = alloted;
+    }
 
-	public String getRc() {
-	    return rc;
-	}
-	 
-	public void setRc(String rc) {
-	    this.rc = rc;
-	}
+    /**
+     * Gets the data of birth.
+     * @return date of birth.
+     */
+    public Date getDb() {
+        return db;
+    }
 
-	public int getTm() {
-	    return tm;
-	}
-	 
-	public void setTm(int tm) {
-	    this.tm = tm;
-	}
+    /**
+     * sets the student's date of birth.
+     * @param db [description]
+     */
+    public void setDb(Date db) {
+        this.db = db;
+    }
 
-	public Student(String studentName, String dateOfBirth,
-	               int m1, int m2, int m3, int tm,
-	               String rc) {
-		this.studentName = studentName;
+    /**
+     * gets the date of birth.
+     * @return the student's date of birth.
+     */
+    public String getRc() {
+        return rc;
+    }
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-		try {
-			this.db = sdf.parse(dateOfBirth);	
-		} catch(Exception ex) {
-			ex.printStackTrace();
-		}
-		
-		this.m1 = m1;
-		this.m2 = m2;
-		this.m3 = m3;
-		this.tm = tm;
-		this.rc = rc;
-		this.alloted = false;
-	}
+    /**
+     * Sets the student's reservation category.
+     * @param rc.
+     */
+    public void setRc(String rc) {
+        this.rc = rc;
+    }
 
-	public int getM3() {
-		return m3;
-	}
+    /**
+     * gets the total marks.
+     * @return returns the total marks of the student.
+     */
+    public int getTm() {
+        return tm;
+    }
 
-	public void setM3(int m3) {
-		this.m3 = m3;
-	}
+    /**
+     * sets the student's total marks.
+     * @param tm.
+     */
+    public void setTm(int tm) {
+        this.tm = tm;
+    }
 
-	public int getM2() {
-		return m2;
-	}
+    /**
+     * Constructs the object based on the parameters.
+     * @param  studentName Student Name
+     * @param  dateOfBirth Date of Birth
+     * @param  m1          Marks in first subject.
+     * @param  m2          Marks in second subject.
+     * @param  m3          Marks in third subject.
+     * @param  tm          Total Marks.
+     * @param  rc          Reservation Category.
+     */
+    public Student(String studentName, String dateOfBirth,
+                   int m1, int m2, int m3, int tm,
+                   String rc) {
+        this.studentName = studentName;
 
-	public void setM2(int m2) {
-		this.m2 = m2;
-	}
+        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+        try {
+            this.db = sdf.parse(dateOfBirth);   
+        } catch(Exception ex) {
+            ex.printStackTrace();
+        }
+        
+        this.m1 = m1;
+        this.m2 = m2;
+        this.m3 = m3;
+        this.tm = tm;
+        this.rc = rc;
+        this.alloted = false;
+    }
 
-	public int getM1() {
-		return m1;
-	}
+    /**
+     * Gets the marks in subject 3.
+     * @return m3 Marks in subject 3.
+     */
+    public int getM3() {
+        return m3;
+    }
 
-	public void setM1(int m1) {
-		this.m1 = m1;
-	}
+    /**
+     * Sets the m3 to the 3rd subject.
+     * @param m3.
+     */
+    public void setM3(int m3) {
+        this.m3 = m3;
+    }
 
-	public String getStudentName() {
-		return studentName;
-	}
+    /**
+     * Gets the marks in subject 2.
+     * @return marks in subject 2.
+     */
+    public int getM2() {
+        return m2;
+    }
 
-	public void setStudentName(String studentName) {
-		this.studentName = studentName;
-	}
+    /**
+     * Sets marks in subject 2.
+     * @param m2.
+     */
+    public void setM2(int m2) {
+        this.m2 = m2;
+    }
 
-	public int compareTo(Student that) {
-		if (this.getTm() > that.getTm()) return -1;
-		if (this.getTm() < that.getTm()) return 1;
+    /**
+     * Gets the marks in subject 1.
+     * @return marks in subject 1.
+     */
+    public int getM1() {
+        return m1;
+    }
 
-		if (this.getM3() > that.getM3()) return -1;
-		if (this.getM3() < that.getM3()) return 1;
+    /**
+     * Sets the marks to subject 1.
+     * @param m1.
+     */
+    public void setM1(int m1) {
+        this.m1 = m1;
+    }
 
-		if (this.getM2() > that.getM2()) return -1;
-		if (this.getM2() < that.getM2()) return 1;
+    /**
+     * Gets the student name.
+     * @return the student's name.
+     */
+    public String getStudentName() {
+        return studentName;
+    }
 
-		if (this.getDb().compareTo(that.getDb()) < 0) return 1;
-		if (this.getDb().compareTo(that.getDb()) > 0) return -1;
-		return 0;
-	}
+    /**
+     * When required the student's name can be updated.
+     * @param studentName.
+     */
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
 
-	public String toString() {
-		StringBuffer sb = new StringBuffer();
-		sb.append(this.getStudentName() + ",");
-		sb.append(this.getTm() + ",");
-		sb.append(this.getRc());
-		// sb.append(this.getAlloted());
-		return sb.toString();
-	}
+    /**
+     * Compares two objects of types students.
+     * @param  that second Student.
+     * @return integer based on the requirements.
+     */
+    public int compareTo(Student that) {
+        if (this.getTm() > that.getTm()) return -1;
+        if (this.getTm() < that.getTm()) return 1;
+
+        if (this.getM3() > that.getM3()) return -1;
+        if (this.getM3() < that.getM3()) return 1;
+
+        if (this.getM2() > that.getM2()) return -1;
+        if (this.getM2() < that.getM2()) return 1;
+
+        if (this.getDb().compareTo(that.getDb()) < 0) return 1;
+        if (this.getDb().compareTo(that.getDb()) > 0) return -1;
+        return 0;
+    }
+
+    /**
+     * Gets the String version of the Students.
+     * @return String version of object.
+     */
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(this.getStudentName() + ",");
+        sb.append(this.getTm() + ",");
+        sb.append(this.getRc());
+        return sb.toString();
+    }
 }
