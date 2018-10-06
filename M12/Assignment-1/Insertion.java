baff4a05b4ab2974f6808e02c1501f6b9feeb21f
ndvssankar <sivashankar@msitprogram.net>
2018-10-07 01:12:49 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M12/Assignment-1/Insertion.java
+++ M12/Assignment-1/Insertion.java
@@ -1,49 +1,55 @@
-
 import java.util.*;
 
-public class Insertion {
+/**
+ * This class demostrates the insertion sort which is used to sort
+ * the comparable elements .
+ * @author Siva Sankar.
+ */
+public final class Insertion {
 
-	private static boolean less(Comparable v, Comparable w) {
-		// System.out.println("V : " + (Team)v + " \t And : " + (Team)w);
-		return v.compareTo(w) < 0;
-	}
+    /**
+     * Insertion sort.
+     */
+    private Insertion() {
 
-	private static void exch(Comparable[] a, int i, int j) {
-		Comparable swap = a[i];
-		a[i] = a[j];
-		a[j] = swap;
-		// System.out.println("Swapped : " + (Team) a[i] + "\t and : " + (Team) a[j]);
-	}
+    }
 
-	/**
-	 * Sorts the elements using insertion sort.
-	 * @param a contains elements that are to be sorted.
-	 */
-	public static void sort(Comparable[] a) {
-		int N = a.length;
-		for (int i = 0; i < N; i++) {
-			for (int j = i; j > 0; j--) {
-				if (less (a[j], a[j-1])) {
-					exch(a, j, j-1);
-				} else
-					break;
-			}
-		}
-	}
+    /**
+     * This method compares two comparable objects and returns
+     * true if the first object is less than second object.
+     * @param  v the first object.
+     * @param  w the second object.
+     * @return   boolean if the first object is less than second object.
+     */
+    private static boolean less(Comparable v, Comparable w) {
+        return v.compareTo(w) < 0;
+    }
+
+    /**
+     * swaps two elements in the specified array.
+     * @param a the array.
+     * @param i the ith index position.
+     * @param j the jth index position.
+     */
+    private static void exch(Comparable[] a, int i, int j) {
+        Comparable swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+    /**
+     * Sorts the elements using insertion sort.
+     * @param a contains elements that are to be sorted.
+     */
+    public static void sort(Comparable[] a) {
+        int N = a.length;
+        for (int i = 0; i < N; i++) {
+            for (int j = i; j > 0; j--) {
+                if (less (a[j], a[j-1])) {
+                    exch(a, j, j-1);
+                } else
+                    break;
+            }
+        }
+    }
 }
-
-
-
-
-
-// England		4,2,3
-// India		5,2,4
-// Australia	6,2,3
-// SouthAfrica	5,3,4
-// Bangladesh	3,4,2
-// Zimbabwe		3,4,6
-// Ireland		3,4,1
-
-
-
-
