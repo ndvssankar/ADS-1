955b491508a24d6522aa30308c252b2363a4d95d
ndvssankar <sivashankar@msitprogram.net>
2018-10-07 01:19:08 +0530

    "ads1_m12_a1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- M12/Assignment-1/Insertion.java
+++ M12/Assignment-1/Insertion.java
@@ -51,7 +51,7 @@ public final class Insertion {
         int N = a.length;
         for (int i = 0; i < N; i++) {
             for (int j = i; j > 0; j--) {
-                if (less (a[j], a[j - 1])) {
+                if (less(a[j], a[j - 1])) {
                     exch(a, j, j - 1);
                 } else {
                     break;
