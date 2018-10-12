4a4ca7bb3b2f54a71a923f0c0eac3043372371d1
ndvssankar <sivashankar@msitprogram.net>
2018-10-12 16:40:43 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- M17/TaxiCabNumbers/Solution.java
+++ M17/TaxiCabNumbers/Solution.java
@@ -1,10 +1,27 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) {
+/**
+ * This class is used to find the nth taxicab numbers
+ * which can be formed with m pairs.
+ * @author Siva Sankar.
+ */
+public final class Solution {
+
+	/**
+	 * Default Constructor.
+	 */
+	private Solution() {
+
+	}
+
+	/**
+	 * To demonstrate the solution.
+	 * @param args command line arguments.
+	 */
+	public static void main(final String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
 		int M = scan.nextInt();
-		System.out.println(Taxicab.findTaxiCab(N, M));
+		System.out.println(Taxicab.findTaxicab(N, M));
 	}
 }
